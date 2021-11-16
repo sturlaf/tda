ccf8f9f3b7160d1466c9305e6bd1c1115bbd281e
Sturla Frydenlund <sturlafr@markov.math.ntnu.no>
2021-11-16 14:22:47 +0100

    trained models

--- train_models.py
+++ train_models.py
@@ -179,6 +179,6 @@ def main(i):
 
 
 if __name__ == "__main__":
-    number_of_cores = cpu_count()
-    Parallel(n_jobs=number_of_cores)(delayed(main)(i) for i in range(100))
+    number_of_cores = 7
+    Parallel(n_jobs=number_of_cores)(delayed(main)(i) for i in range(29, 100))
 
