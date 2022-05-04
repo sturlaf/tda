40dfb4d457a75af3ab1692dd8c1ab01ddd9f6da1
Sturla Frydenlund <sturla.frydenlund@gmail.com>
2022-05-05 00:11:16 +0200

    added a print line to see progress

--- find_circles_mnist.py
+++ find_circles_mnist.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import StandardScaler
 def calculate_persistence(
     cluster, num_of_neurons, maxdim=1, coeff=47, num_longest_bars=10
 ):
+    print(cluster.shape[0])
     if cluster.shape[0] < 4000:
         layout = UMAP(
             n_components=num_of_neurons,
