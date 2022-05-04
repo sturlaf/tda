e9372693035e5f2ef19358ed0b26f8b536d7d1bd
Sturla Frydenlund <sturla.frydenlund@gmail.com>
2022-05-04 18:17:27 +0200

    cleaned up files

--- run_app.py
+++
@@ -1,7 +0,0 @@
-import sys
-from streamlit import cli as stcli
-
-if __name__ == "__main__":
-    # sys.argv = ["streamlit", "run", "sampling.py"]
-    sys.argv = ["streamlit", "run", "mnst_app.py"]
-    sys.exit(stcli.main())
